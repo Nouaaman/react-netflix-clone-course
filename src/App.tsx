ca2dbfb5307b1e2616aca8bde63796239f9a1572
Nouaaman <nouaamank1@gmail.com>
2022-02-23 21:17:01 +0100

    routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
 
-export default () => <div>My App</div>;
+function App() {
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
+}
+export default App
