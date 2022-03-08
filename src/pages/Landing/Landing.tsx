a5767e37c20a3c068b2d9993a73dcca9b1cabdcd
Nouaaman <nouaamank1@gmail.com>
2022-03-08 12:59:56 +0100

    landing page faq

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import './Landing.css'
 
 function Landing() {
 
+    const [isOpened, setOpened] = useState(false);
+
+    const handleToggle = () => {
+        setOpened(!isOpened);
+    };
     return (
         <>
             <header>
@@ -38,7 +43,7 @@ function Landing() {
                         </div>
                         <div className='story-card-image'>
                             <div className="story-card-image-container">
-                                <video src="assets/video-tv.m4v" autoPlay loop ></video>
+                                <video src="assets/video-tv.m4v" autoPlay loop muted ></video>
                                 <img src="assets/tv.png" alt="tv" />
                             </div>
                         </div>
@@ -49,7 +54,7 @@ function Landing() {
                     <div className="story-card-container">
                         <div className='story-card-image'>
                             <div className="story-card-image-container">
-                                <video src="assets/video-tv.m4v" autoPlay loop ></video>
+                                <video src="assets/video-tv.m4v" autoPlay loop muted ></video>
                                 <img src="assets/tv.png" alt="tv" />
                             </div>
                         </div>
@@ -69,8 +74,8 @@ function Landing() {
                         </div>
                         <div className='story-card-image'>
                             <div className="story-card-image-container">
-                                <video src="assets/video-tv.m4v" autoPlay loop ></video>
-                                <img src="assets/tv.png" alt="tv" />
+                                <video src="assets/video-devices.m4v" autoPlay loop muted></video>
+                                <img src="assets/img-devices.png" alt="tv" />
                             </div>
                         </div>
                     </div>
@@ -80,13 +85,46 @@ function Landing() {
                     <div className="story-card-container">
                         <div className='story-card-image'>
                             <div className="story-card-image-container">
-                                <video src="assets/video-tv.m4v" autoPlay loop ></video>
-                                <img src="assets/tv.png" alt="tv" />
+                                <img src="assets/kids.png" alt="tv" />
                             </div>
                         </div>
                         <div className='story-card-text'>
                             <h1>Create profiles for kids.</h1>
                             <h2>Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</h2>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className='story-card faq-card'>
+                    <div className="story-card-container">
+                        <div className='story-card-text'>
+                            <h1>Frequently Asked Questions</h1>
+                            <h2>Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</h2>
+                        </div>
+                        <ul className="faq-list">
+                            <li>
+                                <button className='faq-question' onClick={handleToggle}>
+                                    What is Netflix?
+                                    <i className="fa-solid fa-plus"></i>
+                                </button>
+                                <div className={isOpened ? 'faq-answer opened' : 'faq-answer closed'}>
+                                    <span>
+                                        Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. <br /><br />
+                                        You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <div className='email-form'>
+                            <input type="text" placeholder='Email address' />
+                            <button className='btn-getstarted'>
+                                <span>Get Started</span>
+                                <span>
+                                    <i className="fa-solid fa-angle-right icon"></i>
+                                </span>
+                            </button>
                         </div>
 
                     </div>
