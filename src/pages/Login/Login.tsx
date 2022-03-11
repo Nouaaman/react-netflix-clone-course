3cb2127fffac0a96ed5ac25296e9889d7ba87a0b
Nouaaman <nouaamank1@gmail.com>
2022-03-11 14:27:16 +0100

    login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,20 +8,20 @@ function Home() {
 
     return (
         <main className='login'>
-            {/* <div className=''></div> */}
-            {/* container for form for light datk bg */}
-            <form action="#" className="container">
-                <h1>Sign In</h1>
-                <div className="form-element">
-                    <input type="text" name="username" id="username" required />
-                    <label className="floating-label" htmlFor="username">Email or Phone Number</label>
-                </div>
-                <div className="form-element">
-                    <input type="password" name="password" id="password" required />
-                    <label className="floating-label" htmlFor="password">Password</label>
-                </div>
-                <button className="btn">Sign In</button>
-            </form>
+            <div className='form-container'>
+                <form action="#" className="container">
+                    <h1>Sign In</h1>
+                    <div className="form-element">
+                        <input type="text" name="username" id="username" required />
+                        <label className="floating-label" htmlFor="username">Email or Phone Number</label>
+                    </div>
+                    <div className="form-element">
+                        <input type="password" name="password" id="password" required />
+                        <label className="floating-label" htmlFor="password">Password</label>
+                    </div>
+                    <button className="btn">Sign In</button>
+                </form>
+            </div>
         </main>
 
     )
